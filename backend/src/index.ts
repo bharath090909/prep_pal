e5714c20577291091fcc2d147b1ff4646b9c7423
Bharath <nallagondlabharath159@gmail.com>
2025-09-28 09:56:10 +0530

    Intialize frontend and backend

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import type { Express } from "express";
+import express from "express";
+
+const app: Express = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
